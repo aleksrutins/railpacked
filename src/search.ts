f8f87e72fb714dd9c2a1d42c3b231dd7c54bd113
Aleks Rutins <keeper@farthergate.com>
2025-11-02 00:37:32 -0400

    Improve search

--- src/search.ts
+++ src/search.ts
@@ -4,15 +4,18 @@ const searchBar = document.querySelector("#searchBar") as HTMLInputElement,
   recipes = document.querySelector("#recipes");
 if (searchBar && recipes) {
   const items = Array.from(document.querySelectorAll("recipe-link")).map(
-    (it) => ({ title: it.getAttribute("title"), el: it }),
+    (it) => ({
+      title: it.getAttribute("title"),
+      desc: it.querySelector("p > slot")!.textContent,
+      el: it,
+    }),
   );
 
-  const fuse = new Fuse(items, { keys: ["title"] });
+  const fuse = new Fuse(items, { keys: ["title", "desc"] });
 
   searchBar.addEventListener("keyup", () => {
+    if (searchBar.value.trim() == "") return;
     const results = fuse.search(searchBar.value).map((it) => it.item.el);
-    recipes.replaceChildren(
-      ...(results.length > 0 ? results : items.map((it) => it.el)),
-    );
+    recipes.replaceChildren(...results);
   });
 }
