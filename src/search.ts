e4a5b70fbdedabd1ec402150686fb46105451cd9
Aleks Rutins <keeper@farthergate.com>
2025-11-03 09:52:51 -0500

    fix search

--- src/search.ts
+++ src/search.ts
@@ -3,13 +3,13 @@ import Fuse from "fuse.js";
 const searchBar = document.querySelector("#searchBar") as HTMLInputElement,
   recipes = document.querySelector("#recipes");
 if (searchBar && recipes) {
-  const items = Array.from(document.querySelectorAll("recipe-link")).map(
-    (it) => ({
-      title: it.getAttribute("title"),
-      desc: it.querySelector("p > slot")!.textContent,
-      el: it,
-    }),
-  );
+  const items = Array.from(
+    document.querySelectorAll("#recipes > div.card"),
+  ).map((it) => ({
+    title: it.querySelector("h2 > a")!.textContent,
+    desc: it.querySelector("p")!.textContent,
+    el: it,
+  }));
 
   const fuse = new Fuse(items, { keys: ["title", "desc"] });
 
